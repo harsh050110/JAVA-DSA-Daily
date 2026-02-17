d4666b1e48a1df9f76a605f1a956fd9ac563b199
Harsh Kumar <harshkumar011045@gmail.com>
2026-02-17 20:26:09 +0530

    added

---
+++ vowel.java
@@ -0,0 +1,13 @@
+public class vowel {
+    public static void main(String[] args){
+        String str ="Hello World";
+        int count =0;
+        for(int i =0; i<str.length(); i++){
+            char ch = str.charAt(i);
+            if(ch=='a'|| ch=='e'|| ch=='i'|| ch=='o'|| ch=='u'){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
