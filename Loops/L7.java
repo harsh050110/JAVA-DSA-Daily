5e41fd17f605d1a9e45f93e4b03ffb91332b1ad9
Harsh Kumar <harshkumar011045@gmail.com>
2026-05-03 17:12:15 +0530

    fix

--- Loops/L7.java
+++ Loops/L7.java
@@ -2,11 +2,11 @@ package Loops;
 
 public class L7 {
     public static void main (String[] args){
-        for(int i=1; i<=6; i++){
-            for(int j=1; j<=i; j++){
-                System.out.println("*");
-            }
-            System.out.println();
-        }
+       for( int i = 1; i <= 4; i++){
+        for (int j =1; j <= i; j++){
+            System.out.print("*");
+       }
+        System.out.println();
     }
+}
 }
