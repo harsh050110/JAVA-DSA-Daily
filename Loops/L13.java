1bf9d230ea9ebadd687f8c7e3bce11facbf04e6b
Harsh Kumar <harshkumar011045@gmail.com>
2026-05-04 20:50:33 +0530

    fix

--- Loops/L13.java
+++ Loops/L13.java
@@ -1,9 +1,18 @@
 package Loops;
 
-public class L12 {
+public class L13 {
     public static void main(String[] args){
-            int n=9;
+        int n=9;
         for(int i=1; i<=n; i++){
+            for(int j=1; j<=n-i; j++){
+                System.out.print(".");
+            }
+            for(int j=1; j<=(2*i-1); j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=n-1; i>=1; i--){
             for(int j=1; j<=n-i; j++){
                 System.out.print(".");
             }
