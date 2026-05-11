fad0c15f8ce17cbbe759bf25e8d9a61b6e1d231c
Harsh Kumar <harshkumar011045@gmail.com>
2026-05-11 21:47:09 +0530

    fix

---
+++ Loops/Prime.java
@@ -0,0 +1,32 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class Prime {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+
+        boolean isPrime = true;
+
+        if(n <= 1) {
+            isPrime = false;
+        } else {
+
+            for(int i = 2; i <= Math.sqrt(n); i++) {
+
+                if(n % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if(isPrime)
+            System.out.println(n + " is Prime");
+        else
+            System.out.println(n + " is Not Prime");
+    }
+}
