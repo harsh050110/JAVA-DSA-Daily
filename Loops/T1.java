116d8379e258156adba6e8500f6e2b1de6b87736
Harsh Kumar <harshkumar011045@gmail.com>
2026-05-14 19:49:55 +0530

    add

---
+++ Loops/T1.java
@@ -0,0 +1,38 @@
+package Loops;
+
+public class T1 {
+    public static void main(String[] args){
+       int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6}
+        };
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int[][] transpose = new int[cols][rows];
+
+        // Transpose Logic
+        for(int i = 0; i < rows; i++) {
+
+            for(int j = 0; j < cols; j++) {
+
+                transpose[j][i] = matrix[i][j];
+            }
+        }
+
+        // Print Transpose Matrix
+        System.out.println("Transpose Matrix:");
+
+        for(int i = 0; i < cols; i++) {
+
+            for(int j = 0; j < rows; j++) {
+
+                System.out.print(transpose[i][j] + " ");
+            }
+
+            System.out.println();
+        }
+        
+    }
+}
