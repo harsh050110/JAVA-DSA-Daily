e51cee9936c24713d9bbde9b6b1bf8b91be1f1b3
Harsh Kumar <harshkumar011045@gmail.com>
2026-08-18 23:19:25 +0530

    push

---
+++ Loops/TJ.java
@@ -0,0 +1,11 @@
+package Loops;
+
+public class TJ {
+    public static void main(String[]args){
+        float CurrentRating = 4.5f;
+        float PreviousRating =42.f;
+        System.out.println("Current Rating: "+ CurrentRating);
+        System.out.println("Previous Rating: "+ PreviousRating);
+    }
+    
+}
