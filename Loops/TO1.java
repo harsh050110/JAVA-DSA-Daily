fb86677fa3d8c4109c49482ebd09b13da05a7922
Harsh Kumar <harshkumar011045@gmail.com>
2026-08-19 23:12:02 +0530

    added

---
+++ Loops/TO1.java
@@ -0,0 +1,12 @@
+package Loops;
+
+public class TO1 {
+    public static void main(String[] args){
+        char Name ='A';
+        short Age = 20;
+        Long PhoneNumber = 123456789L;
+        System.out.println("Name: "+Name);
+        System.out.println("Age: "+Age);
+        System.out.println("Phone Number: "+PhoneNumber);
+    }
+}
