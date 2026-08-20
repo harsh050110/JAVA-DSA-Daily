e405d40a9c9196f2177f496cdfa839e76f6ae6ec
Harsh Kumar <harshkumar011045@gmail.com>
2026-08-20 22:01:43 +0530

    push

---
+++ TO2.java
@@ -0,0 +1,11 @@
+public class TO2 {
+    public static void main(String[] args){
+        double a =3456.87;
+        long b = 34566L;
+        boolean c = true;
+
+        System.out.println("Double value: "+a);
+        System.out.println("Long value: "+b);
+        System.out.println("Boolean value: "+c);
+    }
+}
