d795659df6a69a382107fd7816e7515629a6ff87
Harsh Kumar <harshkumar011045@gmail.com>
2026-08-21 22:42:26 +0530

    push

---
+++ TO3.java
@@ -0,0 +1,14 @@
+public class TO3 {
+    public static void main(String[] args){
+        int a =3478,b=7865, c=8921;
+        double e = 3456.87;
+        long f = 34566L;
+        boolean g = true;
+
+        System.out.println("Integer value: "+a);
+        System.out.println("Integer value: "+b);
+        System.out.println("double value: "+e);
+        System.out.println("long value: "+f);
+        System.out.println("boolean value: "+g);
+    }
+}
