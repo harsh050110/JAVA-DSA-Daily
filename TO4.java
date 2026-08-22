c5bf492b07e6022d982a23e6d3e3cafbc8695ded
Harsh Kumar <harshkumar011045@gmail.com>
2026-08-22 22:10:28 +0530

    added

---
+++ TO4.java
@@ -0,0 +1,10 @@
+public class TO4 {
+    public static void main(String[] args){
+        int a = 3876;
+        double b = a;
+        System.out.println("Integer value: "+a);
+        System.out.println("Double value: "+b);
+        
+    }
+    
+}
