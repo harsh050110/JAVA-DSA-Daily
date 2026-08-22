ccfd55dbd4598a857cfc0c8e5a5df6c7cc6c2c20
Harsh Kumar <harshkumar011045@gmail.com>
2026-08-22 22:13:27 +0530

    added

---
+++ TO5.java
@@ -0,0 +1,8 @@
+public class TO5 {
+    public static void main(String[] args){
+        int a = 69_87;
+        long b = (long)a;
+        System.out.println("Integer value: "+a);
+        System.out.println("Long value: "+b);
+    }
+}
