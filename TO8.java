bb63dea799fa39bc5a7eaea3424158497b858d82
Harsh Kumar <harshkumar011045@gmail.com>
2026-08-23 21:57:58 +0530

    push

---
+++ TO8.java
@@ -0,0 +1,8 @@
+public class TO8 {
+    public static void main(String[] args){
+        double a = 69.87;
+        float b = (float)a;
+        System.out.println("Double value: "+a);
+        System.out.println("Float value: "+b);
+    }
+}
