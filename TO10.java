162a0099721b0fa5aae4d8d40a4f03adeaa06ca2
Harsh Kumar <harshkumar011045@gmail.com>
2026-08-24 21:37:34 +0530

    added

---
+++ TO10.java
@@ -0,0 +1,10 @@
+public class TO10 {
+    public static void main(String[]args){
+        int a = 69;
+        int b = 79;
+        int sum;
+        sum = a+b;
+        System.out.println("Sum of two numbers: "+sum);
+    }
+    
+}
