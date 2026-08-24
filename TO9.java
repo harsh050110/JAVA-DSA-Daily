85a69d34b593b041cf038eb59d3ed6ebc6d84110
Harsh Kumar <harshkumar011045@gmail.com>
2026-08-24 21:32:19 +0530

    addeD

---
+++ TO9.java
@@ -0,0 +1,9 @@
+public class TO9 {
+    public static void main(String[] args){
+        final double a =69.87;
+        final int b = (int)a;
+        System.out.println("Double value: "+a);
+        System.out.println("Int value: "+b);
+    }
+    
+}
