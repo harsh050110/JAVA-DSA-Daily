4e9fa5a43a9b27779da1ba205c1f04070648589a
Harsh Kumar <harshkumar011045@gmail.com>
2026-08-25 13:25:41 +0530

    added

---
+++ TO11.java
@@ -0,0 +1,11 @@
+public class TO11 {
+    public static void main(String[]args){
+        int firstFee =8900;
+        int secondFee = 12000;
+        int totalFee = 25000;
+        int ExtraFee =   totalFee-firstFee + secondFee;
+
+        System.out.println("Extra Fee: "+ExtraFee);
+    }
+    
+}
