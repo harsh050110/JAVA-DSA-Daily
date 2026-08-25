f26f487e7888fdbd02bdeacf3067f85960bf7922
Harsh Kumar <harshkumar011045@gmail.com>
2026-08-25 13:28:34 +0530

    added

---
+++ TO112.java
@@ -0,0 +1,10 @@
+public class TO112 {
+    public static void main(String[]args){
+        int ProductPrice = 15000;
+        int Discount = 10000 ;
+        int ExtraFee = 500;
+        int finalPrice=  ProductPrice-Discount*ExtraFee;
+        System.out.println("Final Price: "+finalPrice);
+    }
+    
+}
