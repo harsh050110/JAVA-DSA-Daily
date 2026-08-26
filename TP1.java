125d8ae90d7b913424169d810acb8d6fc0ed7492
Harsh Kumar <harshkumar011045@gmail.com>
2026-08-26 20:54:10 +0530

    added

---
+++ TP1.java
@@ -0,0 +1,13 @@
+public class TP1 {
+    public static void main(String[]args){
+        int student  = 100;
+        int physicsstudent = 30; 
+        int chemistrystudent = 20; 
+
+        int totalstudent = physicsstudent + chemistrystudent;
+        int remainingstudent = student - totalstudent;
+        System.out.println("Remaining Student: "+ remainingstudent);
+        System.out.println("Total Student: "+ totalstudent);
+    }
+    
+}
