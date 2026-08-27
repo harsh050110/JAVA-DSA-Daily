e540821ab80c576da88ed340d454b89c37051382
Harsh Kumar <harshkumar011045@gmail.com>
2026-08-27 21:08:39 +0530

    added

---
+++ TP2.java
@@ -0,0 +1,12 @@
+public class TP2 {
+    public static void main(String[] args){
+        double totalUSD =100.0;
+        double exchangeRate = 98.0;
+        double totalINR = totalUSD * exchangeRate;
+
+        System.out.println("Total INR: "+ totalINR);
+
+
+    }
+    
+}
