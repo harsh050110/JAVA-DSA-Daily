a7fa88e3466a99fbabe68fa6052266166e5d467d
Harsh Kumar <harshkumar011045@gmail.com>
2026-08-28 20:04:52 +0530

    added

---
+++ TP4.java
@@ -0,0 +1,12 @@
+public class TP4{
+    public static void main(String[] args){
+        int num1 = 20;
+        int num2= 45;
+        int num3= 4;
+        int num4 = 9;
+        int remainder = num1 % num3;
+        int division = num2 /num4;
+        System.out.println("Remainder: "+ remainder);
+        System.out.println("Division: "+ division);
+    }
+}
