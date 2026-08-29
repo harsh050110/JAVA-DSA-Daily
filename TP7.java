46a84d542798419a9128e2e6fafcf706aeacc01a
Harsh Kumar <harshkumar011045@gmail.com>
2026-08-29 21:42:04 +0530

    added

---
+++ TP7.java
@@ -0,0 +1,17 @@
+public class TP7 {
+    public static void main(String[] args){
+        int averageSpeed = 60;
+
+        String name = "BMW";
+        int averageSpeed2 = 120;
+
+        int Distance = 240;
+
+        int Speed = Distance / averageSpeed2;
+
+
+        System.out.println("BMW Average Speed: "+ Speed +"Km/hr");
+
+    }
+    
+}
