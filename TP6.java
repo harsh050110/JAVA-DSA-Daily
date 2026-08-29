60f852e143b3f1c7731c047cc7d9c6a8851e4b48
Harsh Kumar <harshkumar011045@gmail.com>
2026-08-29 21:29:44 +0530

    added

---
+++ TP6.java
@@ -0,0 +1,16 @@
+public class TP6 {
+    public static void main(String[] args){
+        int totalMoney = 1000;
+
+        int moneySpent = 250;
+
+        int netMoney = 250;
+
+        netMoney = totalMoney - moneySpent;
+
+        System.out.println("Total Money: "+ netMoney);
+
+
+    }
+    
+}
