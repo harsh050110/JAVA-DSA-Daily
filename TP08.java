f78d5bed546f28f31e10823297e82689d4129567
Harsh Kumar <harshkumar011045@gmail.com>
2026-08-30 21:23:48 +0530

    added

---
+++ TP08.java
@@ -0,0 +1,10 @@
+public class TP08 {
+    public static void main(String[] args){
+
+        float finalDistance = 240.3f;
+        float InitialDistance = 87.9f;
+        finalDistance -= InitialDistance;
+        System.out.println(finalDistance);
+    }
+    
+}
