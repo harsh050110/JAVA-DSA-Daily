5e70122ff1a8f6cadfbe9edce3da6297aadacb5b
Harsh Kumar <harshkumar011045@gmail.com>
2026-08-31 11:57:34 +0530

    added

---
+++ TO20.java
@@ -0,0 +1,17 @@
+public class TO20 {
+    public static void main(String[] args){
+        String name = "Harsh";
+        int age =20;
+        int marks = 90;
+
+
+        String name2 ="Rishika";
+        int age2 = 21;
+        int marks2 =80;
+
+        System.out.println("Name: "+name+"Age: "+age+"Marks: "+marks);
+        System.out.print("Name: "+name2+"Age: "+age2+"Marks: "+marks2);
+
+
+    }
+}
