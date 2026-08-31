f7e7a189adbb0666ca40f030abf9199694439b43
Harsh Kumar <harshkumar011045@gmail.com>
2026-08-31 11:50:00 +0530

    added

---
+++ TP09.java
@@ -0,0 +1,13 @@
+public class TP09 {
+    public static void main(String[] args){
+
+        float finalMarks = 340.4f;
+        float InitialMarks = 87.9f;
+
+        finalMarks -= InitialMarks;
+
+        System.out.println("The final marks are: "+ finalMarks);
+
+    }
+    
+}
