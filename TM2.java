68ed9a4747226fd5a0f59003436b8efe21fb621f
Harsh Kumar <harshkumar011045@gmail.com>
2026-09-01 20:35:34 +0530

    added

---
+++ TM2.java
@@ -0,0 +1,12 @@
+public class TM2 {
+    public static void main(String[] args){
+        int[] arr1 = {2,3,4,5,6,7};
+        int[] arr2 = {1,2,3,4,5,6};
+
+        int sum = arr1[0] + arr2[2];
+
+        System.out.println(sum);
+    
+        
+    }
+}
