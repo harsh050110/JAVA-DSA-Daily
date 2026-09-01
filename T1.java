af1af9bca2702fcec9356844cf74ce61e3b39710
Harsh Kumar <harshkumar011045@gmail.com>
2026-09-01 20:12:20 +0530

    added

---
+++ T1.java
@@ -0,0 +1,14 @@
+public class T1 {
+    public static void main(String[] args){
+        int a = 5, b = 5;
+
+        int c = ++a * 2;
+        int d = b++ * 2;
+
+         System.out.println(c); 
+        //  this is meaning of the adding the value of a first then use it . a=5 +1 = 6 * 2 = 12;
+
+        System.out.println(d);
+    }
+    
+}
