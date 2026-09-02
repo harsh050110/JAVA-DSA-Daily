ea9c1151f63a078bb2860ef3a60fbbd8c5d02fa5
Harsh Kumar <harshkumar011045@gmail.com>
2026-09-02 21:05:50 +0530

    added

---
+++ TP12.java
@@ -0,0 +1,16 @@
+public class TP12 {
+    public static void main(String[]args){
+        int productA = 320;
+        int productB = 250;
+        int qualityA =4;
+        int qualityB = 5;
+
+        if(productA>productB && qualityA>qualityB){
+            
+            System.out.println("Product A is better than Product B");
+        }else{
+            System.out.println("Product B is better than Product A");
+        }
+    }
+    
+}
