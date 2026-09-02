eedc2b78a2985fc2a962593420d9f0cb3cb6c9a0
Harsh Kumar <harshkumar011045@gmail.com>
2026-09-02 20:59:19 +0530

    added

---
+++ TP11.java
@@ -0,0 +1,16 @@
+public class TP11 {
+    public static void main(String[] args){
+        int arr1 = 150;
+        int arr2 =190;
+
+        if (arr1>arr2){
+            
+        
+            System.out.println("arr1 is greater than arr2");
+        }else{
+            System.out.println("arr2 is greater than arr1");
+        }
+        
+    }
+    
+}
