fb86fffd85c459c9639ddb735309aa6dfc6f7516
Harsh Kumar <harshkumar011045@gmail.com>
2026-09-03 21:59:04 +0530

    added

---
+++ TP19.java
@@ -0,0 +1,10 @@
+public class TP19 {
+    public static void main(String[] args){
+        String s1 ="Hello";
+        String s2 ="hello";
+
+        System.out.println(s1.equalsIgnoreCase(s2));
+
+    }
+    
+}
