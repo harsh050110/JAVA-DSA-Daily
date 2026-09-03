b4f6410e58a70182e37f2c243f9c3e502bb0f3bd
Harsh Kumar <harshkumar011045@gmail.com>
2026-09-03 21:23:47 +0530

    aaded

---
+++ TP16.java
@@ -0,0 +1,12 @@
+public class TP16 {
+    public static void main(String[] args){
+        String firstName = "William";
+        String lastName = "Smith";
+
+        String fullName = firstName.concat(lastName);
+
+        System.out.println("Full Name: "+fullName);
+
+    }
+    
+}
