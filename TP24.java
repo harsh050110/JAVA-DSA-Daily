c19e9e51611e4118c76d04b60110612655297ee6
Harsh Kumar <harshkumar011045@gmail.com>
2026-09-05 10:00:24 +0530

    added

---
+++ TP24.java
@@ -0,0 +1,21 @@
+public class TP24 {
+    public static void main(String[]args){
+        int[] arr1 = {2,3,87,90};
+        int[] arr2 = {7,8,9,100,189};
+
+
+        // comparison
+
+        if(arr1[0]>arr2[4])
+            
+                System.out.println("the number will be greater");
+        else{
+            System.out.println("Not greater than that");
+        }
+
+        int sum = arr1[3]+arr2[4];
+
+        System.out.println("The Total Sum: "+sum);
+    }
+    
+}
