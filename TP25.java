5d635314ca1012d58cd48945febedf8420b98297
Harsh Kumar <harshkumar011045@gmail.com>
2026-09-05 17:32:22 +0530

    added

---
+++ TP25.java
@@ -0,0 +1,13 @@
+public class TP25 {
+    public static void main(String[]args){
+        int age = 25;
+
+        if(age>18){
+            System.out.println("You're adult now");
+
+        }else{
+            System.out.println("You're not adult yet");
+        }
+    }
+    
+}
