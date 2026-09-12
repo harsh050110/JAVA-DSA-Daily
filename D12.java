bd2e68be3848996a4da4c0ad053816cee23e3550
Harsh Kumar <harshkumar011045@gmail.com>
2026-09-12 19:40:23 +0530

    added

--- D12.java
+++ D12.java
@@ -3,6 +3,7 @@ public class D12 {
         pattern1(4);
         pattern2(5);
         pattern3(6);
+        pattern4(5);
     }
     static void pattern1(int n){
         for(int i=1; i<=n; i++){
@@ -24,6 +25,14 @@ public class D12 {
         for(int i=1; i<=n; i++){
             for(int j=1; j<=n; j++){
                 System.out.print("#");
+            }
+            System.out.println();
+        }
+    }
+    static void pattern4(int n){
+        for(int i=1; i<=5; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(i);
             }
             System.out.println();
         }
