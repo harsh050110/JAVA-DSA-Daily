354f67fe79c86042487ed6c87ddfa0afd96feb44
Harsh Kumar <harshkumar011045@gmail.com>
2026-09-13 22:15:33 +0530

    Added

--- D16.java
+++ D16.java
@@ -3,11 +3,20 @@ import java.util.*;
 public class D16 {
     public static void main(String[] args){
         pattern1(5);
+        pattern2(6);
     }
     static void pattern1(int n){
         for(int i=0; i<=n; i++){
             for(int j=0; j<=i; j++){
                 System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    static void pattern2(int n){
+        for(int i=0; i<=n; i++){
+            for(int j=0; j<=n; j++){
+                System.out.print("%");
             }
             System.out.println();
         }
