dbee12686a96401df1adbbdf587ab4842cb72652
Harsh Kumar <harshkumar011045@gmail.com>
2026-09-13 22:19:38 +0530

    added

--- D16.java
+++ D16.java
@@ -5,6 +5,7 @@ public class D16 {
         pattern1(5);
         pattern2(6);
         pattern3(4);
+        pattern4(4);
     }
     static void pattern1(int n){
         for(int i=0; i<=n; i++){
@@ -26,6 +27,14 @@ public class D16 {
         for(int i=0; i<=n; i++){
             for(int j=0; j<=n-i; j++){
                 System.out.print("#");
+            }
+            System.out.println();
+        }
+    }
+    static void pattern4(int n){
+        for(int i=0; i<=n; i++){
+            for(int j=0; j<=i; j++){
+                System.out.print("@");
             }
             System.out.println();
         }
