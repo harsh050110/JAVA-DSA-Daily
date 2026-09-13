fcc776e62f3f6c6c3f69cf1cf800760a73682fef
Harsh Kumar <harshkumar011045@gmail.com>
2026-09-13 22:17:13 +0530

    added

--- D16.java
+++ D16.java
@@ -4,6 +4,7 @@ public class D16 {
     public static void main(String[] args){
         pattern1(5);
         pattern2(6);
+        pattern3(4);
     }
     static void pattern1(int n){
         for(int i=0; i<=n; i++){
@@ -17,6 +18,14 @@ public class D16 {
         for(int i=0; i<=n; i++){
             for(int j=0; j<=n; j++){
                 System.out.print("%");
+            }
+            System.out.println();
+        }
+    }
+    static void pattern3(int n){
+        for(int i=0; i<=n; i++){
+            for(int j=0; j<=n-i; j++){
+                System.out.print("#");
             }
             System.out.println();
         }
