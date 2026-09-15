541e0378d060024b4b6fb92c2bab3263a8e338df
Harsh Kumar <harshkumar011045@gmail.com>
2026-09-15 20:29:31 +0530

    Added

--- D19.java
+++ D19.java
@@ -31,7 +31,19 @@ public class D19 {
                 System.out.print("*");
             }
 
+            
+
         System.out.println();
         }
+        for(int i=5; i>=1; i--){
+            for(int j=5; j>i; j--){
+                System.out.print("");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
+
 }
