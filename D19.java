dfce4b5559b5fbacd8e648518c98d068382685b6
Harsh Kumar <harshkumar011045@gmail.com>
2026-09-16 13:15:12 +0530

    added

--- D19.java
+++ D19.java
@@ -2,6 +2,9 @@ public class D19 {
     public static void main(String[] args) {
         pattern1(8);
         pattern2(8);
+        pattern3(5);
+        pattern4(5);
+        pattern5(5);
     }
 
     static void pattern1(int n) {
@@ -45,5 +48,45 @@ public class D19 {
             System.out.println();
         }
     }
+    static void pattern3(int n){
+    for(int i=n; i>=1; i--){
+        for(int j=1; j<=i; j++){
+            System.out.print("*");
+        }
+
+        for(int j=1; j<=2*(n-i); j++){
+            System.out.print(" ");
+        }
+
+        for(int j=1; j<=i; j++){
+            System.out.print("*");
+        }
+
+        System.out.println();
+    }
+}
+static void pattern4(int n){
+    for(int i=n; i>=1; i--){
+        for(int j=1; j<=i; j++){
+            System.out.print("*");
+        }
+        for(int j=1; j<=2*(n-i); j++){
+            System.out.print(" ");
+        }
+        for(int j=1; j<=i; j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
+static void pattern5(int n){
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            System.out.print("8");
+        }
+        System.out.println();
+    }
+}
+
 
 }
