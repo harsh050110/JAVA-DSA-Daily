3c3a2eaa34b0d881182a437f797e202683bdcf9a
Harsh Kumar <harshkumar011045@gmail.com>
2026-09-17 23:06:02 +0530

    added

--- D20.java
+++ D20.java
@@ -42,5 +42,17 @@ public class D20 {
             }
             System.out.println();
         }
+        for(int i=n; i>=1; i--){
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            for(int j=1; j<=2*(n-i); j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
