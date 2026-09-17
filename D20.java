67fd12aca80a499a810959f8ec6d84365e669eec
Harsh Kumar <harshkumar011045@gmail.com>
2026-09-17 23:05:22 +0530

    added

--- D20.java
+++ D20.java
@@ -1,6 +1,7 @@
 public class D20 {
     public static void main(String[] args){
         pattern1(8);
+        pattern2(5);
     }
     static void pattern1(int n){
         for(int i=1; i<=n; i++){
@@ -27,5 +28,19 @@ public class D20 {
     }
     System.out.println();
 }
+    }
+    static void pattern2(int n){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            for(int j=1; j<=2*(n-i); j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
