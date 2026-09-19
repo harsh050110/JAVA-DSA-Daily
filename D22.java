0087b3e6d8e891b4c45321f88f01e39596486c36
Harsh Kumar <harshkumar011045@gmail.com>
2026-09-19 20:02:48 +0530

    added

--- D22.java
+++ D22.java
@@ -2,6 +2,7 @@ public class D22 {
     public static void main(String[] args){
         pattern1(5);
         pattern2(4);
+        pattern3(3);
     }
     static void pattern1(int n){
         for(int i=1; i<=n; i++){
@@ -15,6 +16,14 @@ public class D22 {
         for(int i=3; i>=1; i--){
             for(int j=1;j<=i; j++){
                     System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    static void pattern3(int n){
+        for(int i=1; i<=n; i++){
+            for(int j=4; j>=i; j--){
+                System.out.print("$");
             }
             System.out.println();
         }
