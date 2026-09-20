234d43eb2731d1a81e27abcda397b30d5dcf9621
Harsh Kumar <harshkumar011045@gmail.com>
2026-09-20 10:39:56 +0530

    added

--- D23.java
+++ D23.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class D23{
     public static void main(String[] args){
-        age(6);
+        shop(5);
     }
     static void age(int n){
         int age;
@@ -17,5 +17,26 @@ public class D23{
         }else{
             System.out.println("You are old age");
         }
+    }
+    static void shop(int n){
+        Scanner sc = new Scanner(System.in);
+        String question = "Enter you total bill: ";
+        System.out.print(question);
+        int totalBill = sc.nextInt();
+        String question1 = "Enter your total items: ";
+        System.out.print(question1);
+        int totalItems = sc.nextInt();
+        int averageBill;
+                averageBill = totalBill/totalItems;
+                System.out.println("Your average bill is: "+averageBill);
+
+                if(averageBill>10000){
+                    System.out.println("You are a premium customer");
+
+                }else if(averageBill<5000){
+                    System.out.println("You are a regular customer");
+                }else{
+                    System.out.println("You are a normal customer");
+                }
     }
 }
