11aa96a62e650248622fdffe892596a29743faa6
Harsh Kumar <harshkumar011045@gmail.com>
2026-09-21 20:10:14 +0530

    AddeD

--- D23.java
+++ D23.java
@@ -2,7 +2,9 @@ import java.util.*;
 
 public class D23{
     public static void main(String[] args){
-        shop(5);
+        // shop(5);
+        Hotel(6);
+        // age(6);
     }
     static void age(int n){
         int age;
@@ -33,6 +35,25 @@ public class D23{
                 if(averageBill>10000){
                     System.out.println("You are a premium customer");
 
+                }else if(averageBill<5000){
+                    System.out.println("You are a regular customer");
+                }else{
+                    System.out.println("You are a normal customer");
+                }
+    }
+    static void Hotel(int n){
+        Scanner sc = new Scanner(System.in);
+        String question = "Enter your total room booked: ";
+        System.out.print(question);
+        int totalRoom = sc.nextInt();
+        String question1 = "Enter your total bill: ";
+        System.out.print(question1);
+        int totalBill = sc.nextInt();
+        int averageBill;
+                averageBill = totalBill/totalRoom;
+                System.out.println("Your average bill is: "+averageBill);    
+                if(averageBill>10000){
+                    System.out.println("You are a premium customer");
                 }else if(averageBill<5000){
                     System.out.println("You are a regular customer");
                 }else{
