27687b7ad689c031fd5abd35f83d6dc56dec9257
Harsh Kumar <harshkumar011045@gmail.com>
2026-09-21 21:25:36 +0530

    addeD

--- D24.java
+++ D24.java
@@ -2,6 +2,7 @@ public class D24 {
     public static void main(String[] args){
         pattern1(5);
         pattern2(7);
+        pattern3(7);
     }
     static void pattern1(int n){
         for(int i=1; i<=n; i++){
@@ -15,6 +16,14 @@ public class D24 {
         for(int i=1; i<=n; i++){
             for(int j=0; j<i; j++){
                 System.out.print((char)('A'+j));
+            }
+            System.out.println();
+        }
+    }
+    static void pattern3(int n){
+        for(int i =7; i>=1; i--){
+            for(int j=1; j<=i; j++){
+                System.out.print((char)('A'+j-1));
             }
             System.out.println();
         }
