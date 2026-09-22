2e1ce4bcd50f7573958f44cc26aeef4da317abb0
Harsh Kumar <harshkumar011045@gmail.com>
2026-09-22 19:10:57 +0530

    added

---
+++ D25.java
@@ -0,0 +1,26 @@
+public class D25 {
+    public static void main(String[] args){
+        int time = 2;
+
+        switch(time){
+            case 1:
+                System.out.println("7AM to 10AM - Breakfast Time");
+                break;
+                case 2:
+                System.out.println("10AM to 12PM - Brunch Time");
+                break;
+                case 3:
+                System.out.println("12PM to 3PM - Lunch Time");
+                break;
+                case 4:
+                System.out.println("3PM to 5PM - Tea Time");
+                break;
+                case 5:
+                System.out.println("5PM to 8PM - Dinner Time");
+                break;
+                default:
+                System.out.println("Invalid Time");
+        }
+    }
+    
+}
