543840b8c55fe4ab6a6c47cd0f98d6f67ba7647b
Harsh Kumar <harshkumar011045@gmail.com>
2026-09-25 19:30:26 +0530

    added

--- D28.java
+++ D28.java
@@ -1,10 +1,19 @@
 public class D28 {
     public static void main(String[] args){
         pattern1(5);
+        pattern2(3);
     }
     static void pattern1(int n){
         for(int i=0;i<n;i++){
             for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    static void pattern2(int n){
+        for(int i=0;i<n;i++){
+            for(int j=n;j>i;j--){
                 System.out.print("*");
             }
             System.out.println();
